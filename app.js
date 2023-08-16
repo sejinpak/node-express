8dfe098711a84d72ae3ec1c74a1d1712adf35ad5
sejinpak <142364864+sejinpak@users.noreply.github.com>
2023-08-16 10:05:41 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,46 +1,18 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-const nunjucks = require('nunjucks');
-
-var indexRouter = require('./routes/index');
-// var usersRouter = require('./routes/users');
-
-var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'njk');
-nunjucks.configure('views', { 
-  express: app,
-  watch: true,
-});
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-// app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
+const express = require('express')
+const app = express()
+app.listen(3000,()=>{
+    console.log('서버온')
+})
+app.get('/search/:name/:code',(req,res)=>{
+    let { name,code } = req.params;
+    fetch('https://open.neis.go.kr/hub/schoolInfo?KEY=49dccbb15f414852936a5870bd0a0545&Type=json&SCHUL_NM='+name+'&ATPT_OFCDC_SC_CODE='+code).then((response) => response.json())//읽어온 데이터를 json으로 변환
+    .then((json) => {
+        res.json(json)
+    });
+})
+app.get('/',(req,res)=>{
+    res.sendFile(__dirname + '/index.html')
+})
 app.use(function(req, res, next) {
-  next(createError(404));
+    res.status(404).sendFile(__dirname+'/notfind.html')
 });
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-module.exports = app;
